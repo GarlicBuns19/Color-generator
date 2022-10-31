2a06829f8efbd3100651c7f07ac6c0d961d62353
GarlicBuns19 <planterx26@gmail.com>
2022-10-31 10:45:20 +0200

    Cant move it

--- color.js
+++ color.js
@@ -1,6 +1,6 @@
 const createBtn = document.getElementById("createBtn");
 const coords = document.getElementById("coords");
-const colorDiv = document.getElementById("colorDiv");
+const colorDiv = document.querySelector(".colorDiv");
 
 function colorCreation() {
   let letter = "0123456789ABCDEF";
@@ -26,7 +26,7 @@ function numberOfColors() {
     colorArr.push({ colObj });
   }
   console.log(colorArr);
-  
+
   colorArr.forEach((color) => {
     colorDiv.innerHTML += `<div id="colorBg${color.colObj.i}" style = "margin: 10px auto;
     padding: 10px;
@@ -36,13 +36,25 @@ function numberOfColors() {
     border-radius: 50%;"></div>`;
     // console.log(color.colObj.color);
   });
-  console.log(colorBg1.getBoundingClientRect().x)
+  // console.log(colorBg1.getBoundingClientRect())
+  // console.log(colorBg2.getBoundingClientRect())
 }
 
-document.addEventListener('mousemove',(e) => {
-  coords.innerHTML = `x : ${e.pageX} / y : ${e.pageY}`
-})
+function colorCoords(id) {
+  console.log(id.x);
+}
+
+document.addEventListener("mousemove", (e) => {
+  coords.innerHTML = `x : ${e.pageX} / y : ${e.pageY}`;
+
+  colorDiv.addEventListener("click", (event) => {
+    // let id = event.target.id
+    let id = event.target.id;
+    id.style.left = e.pageX + "px"
+    // console.log(id.getBoundingClientRect())
+  });
+});
+
 createBtn.addEventListener("click", () => {
   numberOfColors();
 });
-
